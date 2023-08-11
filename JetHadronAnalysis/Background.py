0cc88dcc520d935b343c6bf8f4b726619c71d6b1
Patrick Steffanic <collidingpaticles@gmail.com>
2023-08-11 15:03:38 -0400

    Adding background error function

--- JetHadronAnalysis/Background.py
+++ JetHadronAnalysis/Background.py
@@ -1,8 +1,8 @@
-from ROOT import TH1 # type: ignore
+from ROOT import TH1D # type: ignore
 from JetHadronAnalysis.Types import AnalysisType
 
 class BackgroundFunction:
-    def __init__(self, backgroundCorrelationFunction: TH1, analysisType:AnalysisType) -> None:
+    def __init__(self, backgroundCorrelationFunction: TH1D, analysisType:AnalysisType) -> None:
         self.analysisType = analysisType
         self.backgroundCorrelationFunction = backgroundCorrelationFunction
 
@@ -18,6 +18,16 @@ class BackgroundFunction:
         else:
             raise NotImplementedError("Analysis type not yet implemented")
         
+    def error(self, deltaPhi: float):
+        '''
+        Returns the error on the background function evaluated at deltaPhi
+        '''
+        # TODO add in RPF stuff, TBD what that is
+        if self.analysisType == AnalysisType.PP:
+            return self.ppBackgroundError(deltaPhi)
+        else:
+            raise NotImplementedError("Analysis type not yet implemented")
+        
     def ppBackgroundFunction(self):
         '''
         Returns the background function evaluated at deltaPhi in proton-proton collisions
@@ -27,3 +37,18 @@ class BackgroundFunction:
             self.backgroundAverage = self.backgroundCorrelationFunction.Integral() / self.backgroundCorrelationFunction.GetNbinsX()
         # return the average value
         return self.backgroundAverage
+    
+    def ppBackgroundError(self, deltaPhi: float):
+        '''
+        Returns the error on the background function evaluated at deltaPhi in proton-proton collisions
+        '''
+        # if this is the first time the function is being called, compute the error on the avcerage value of the background correlation function
+        if not hasattr(self, "backgroundError"):
+            sumOfSquaredErrors = 0
+            for i in range(1, self.backgroundCorrelationFunction.GetNbinsX() + 1):
+                sumOfSquaredErrors += self.backgroundCorrelationFunction.GetBinError(i)**2
+            unbiasedNorm = self.backgroundCorrelationFunction.GetNbinsX() * (self.backgroundCorrelationFunction.GetNbinsX() - 1)
+            self.backgroundError = (sumOfSquaredErrors / unbiasedNorm)**0.5
+
+        # return the error on the average value
+        return self.backgroundError
