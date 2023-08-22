6e07490dd781c37e1e2f5c3b78c1996756a537aa
Patrick Steffanic <collidingpaticles@gmail.com>
2023-08-22 16:13:17 -0400

    Script that essentially fills the PID db

--- getParticleFractions.py
+++ getParticleFractions.py
@@ -5,15 +5,12 @@ from JetHadronAnalysis.Types import ParticleType as pt
 
 if __name__=="__main__":
     ana_pp = Analysis(at.PP, ["/mnt/d/pp/17p.root"])
-    ana_pp.setAssociatedHadronMomentumBin(AssociatedHadronMomentumBin.PT_1_15)
-    ana_pp.setRegion(Region.NEAR_SIDE_SIGNAL)
-    print(f"Near-side signal, {ana_pp.getPIDFractions()}")
-    ana_pp.setRegion(Region.AWAY_SIDE_SIGNAL)
-    print(f"Away-side signal, {ana_pp.getPIDFractions()}")
-    ana_pp.setAssociatedHadronMomentumBin(AssociatedHadronMomentumBin.PT_15_2)
-    ana_pp.setRegion(Region.NEAR_SIDE_SIGNAL)
-    print(f"Near-side signal, {ana_pp.getPIDFractions()}")
-    ana_pp.setRegion(Region.AWAY_SIDE_SIGNAL)
-    print(f"Away-side signal, {ana_pp.getPIDFractions()}")
-    
+    for assoc_bin in AssociatedHadronMomentumBin:
+        ana_pp.setAssociatedHadronMomentumBin(assoc_bin)
+        ana_pp.setRegion(Region.NEAR_SIDE_SIGNAL)
+        print(f"Near-side signal, {ana_pp.getPIDFractions()}")
+        ana_pp.setRegion(Region.AWAY_SIDE_SIGNAL)
+        print(f"Away-side signal, {ana_pp.getPIDFractions()}")
+        ana_pp.setRegion(Region.BACKGROUND)
+        print(f"Background, {ana_pp.getPIDFractions()}")
     
